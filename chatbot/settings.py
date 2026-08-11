23fe68e0d3bb04c9e785d8354e2db456686b0d6d
Akshintala sai shashank <163711400+saishashank14@users.noreply.github.com>
2026-08-11 10:13:53 +0530

    Update settings.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
